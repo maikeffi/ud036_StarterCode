c392b090d866a02068755261261464cc982fead6
Kannan Maikeffi <maikeffi@gmail.com>
2017-09-10 09:10:05 +0530

    completed media.py and entertainment_center.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
+
+type 'python entertainment_center.py'
+command to launch the website on webbrowser
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,11 +1,13 @@
 import media as md
 import fresh_tomatoes
 
-pfiction = md.Trailer("Pulp Fiction","The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption"
-                      ,"https://i.imgur.com/3llONDs.png",
+#initiate instance for movies
+pfiction = md.Trailer("Pulp Fiction", "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption"
+                      , "https://i.imgur.com/3llONDs.png",
                       "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
-rDogs = md.Trailer("Reservoir Dogs","After a simple jewelry heist goes terribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
+rDogs = md.Trailer("Reservoir Dogs",
+                   "After a simple jewelry heist goes terribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
                    "https://i0.wp.com/www.la-screenwriter.com/wp-content/uploads/2013/04/reservoir_dogs_1992_8.jpg?resize=682%2C1024",
                    "https://www.youtube.com/watch?v=vayksn4Y93A")
 
@@ -23,8 +25,9 @@ dknight = md.Trailer("The Dark Knight",
                      "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
                      "https://i.imgur.com/oRueeKN.png",
                      "https://www.youtube.com/watch?v=EXeTwQWrcwY")
+#creates a list of movie for the website with the data created above
 movies = [pfiction,rDogs,scarface,serpico,dknight]
 
-
+#launches the website
 fresh_tomatoes.open_movies_page(movies)
 
